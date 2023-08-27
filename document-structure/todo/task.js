e2931142c428959cbe4df729f1ce757c226fe1d8
Djama <solverm85@gmail.com>
2023-08-28 01:09:45 +0300

    Changes edded commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,46 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const taskInput = document.getElementById("task__input");
-    const addButton = document.getElementById("tasks__add");
-    const tasksList = document.getElementById("tasks__list");
-  
-    taskInput.addEventListener("keypress", function (event) {
-      if (event.key === "Enter" && taskInput.value.trim() !== "") {
-        event.preventDefault();
-        addTask(taskInput.value.trim());
-        taskInput.value = "";
-      }
-    });
-  
-    addButton.addEventListener("click", function () {
-      if (taskInput.value.trim() !== "") {
-        addTask(taskInput.value.trim());
-        taskInput.value = "";
-      }
-    });
-  
-    tasksList.addEventListener("click", function (event) {
-      if (event.target.classList.contains("task__remove")) {
-        event.preventDefault();
-        event.target.parentNode.remove();
-      }
-    });
-  
-    function addTask(title) {
-      const taskElement = document.createElement("div");
-      taskElement.classList.add("task");
-  
-      const taskTitleElement = document.createElement("div");
-      taskTitleElement.classList.add("task__title");
-      taskTitleElement.innerText = title;
-  
-      const removeLink = document.createElement("a");
-      removeLink.classList.add("task__remove");
-      removeLink.innerHTML = "&times;";
-  
-      taskElement.appendChild(taskTitleElement);
-      taskElement.appendChild(removeLink);
-  
-      tasksList.appendChild(taskElement);
+  const input = document.getElementById("task-input");
+  const addButton = document.getElementById("add-button");
+  const taskList = document.getElementById("task-list");
+
+  addButton.addEventListener("click", function () {
+    const taskText = input.value.trim();
+
+    if (taskText !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+      taskList.appendChild(taskItem);
+      input.value = "";
     }
   });
-  
+
+  const form = document.getElementById("task-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    const taskText = input.value.trim();
+
+    if (taskText !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+      taskList.appendChild(taskItem);
+      input.value = "";
+    }
+  });
+});
